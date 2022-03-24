6b94beaa6928bd76d6bf23d28a25710227bc1d32
Basith P <basithpofficial@gmail.com>
2022-03-24 20:50:43 +0530

    Parts hoizontal list - details page

--- lib/view/details/details_page.dart
+++ lib/view/details/details_page.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:orilla_fresca/config/theme/colors.dart';
 import 'package:orilla_fresca/models/sub_category.dart';
+import 'package:orilla_fresca/view/cat_list/widgets/category_icon.dart';
 import 'package:orilla_fresca/widgets/main_app_bar.dart';
 
 class DetailsPage extends StatefulWidget {
@@ -15,10 +19,159 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
+    final subCat = widget.subcategory;
     return Scaffold(
-      appBar: const MainAppBar(),
-      body: Center(
-        child: Text(widget.subcategory.name),
+      body: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(45)),
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                SizedBox(
+                  height: 300,
+                  child: Image.asset(
+                    'assets/imgs/${subCat.imgName}_desc.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned.fill(
+                  child: Container(color: Colors.black38),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CategoryIcon(color: subCat.color, iconName: subCat.icon),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          const Text(
+                            'Pork Meat',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Chip(
+                            label: const Text(
+                              '\$200/kg',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            elevation: 0,
+                            backgroundColor: subCat.color,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  right: 20,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: AppColors.primary,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: const [
+                        Text(
+                          '3',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.white,
+                          size: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: MainAppBar(
+                    themeColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  child: Text(
+                    'Select items',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 190,
+                  child: ListView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: subCat.parts.length,
+                    itemBuilder: (context, i) => GestureDetector(
+                      onTap: () => setState(() {
+                        for (var part in subCat.parts) {
+                          part.isSelected = subCat.parts[i] == part;
+                        }
+                      }),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            height: 140,
+                            width: 130,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: subCat.parts[i].isSelected
+                                  ? Border.all(color: subCat.color, width: 2)
+                                  : null,
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 10,
+                                  offset: Offset.zero,
+                                ),
+                              ],
+                              image: DecorationImage(
+                                image: AssetImage('assets/imgs/${subCat.parts[i].imgName}.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            subCat.parts[i].name,
+                            style: TextStyle(color: subCat.color),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
