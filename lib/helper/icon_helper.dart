b67ab5b3e3d8224a292607997ed146be200e8bc0
Basith P <basithpofficial@gmail.com>
2022-03-24 11:17:21 +0530

    Add icon helper class

---
+++ lib/helper/icon_helper.dart
@@ -0,0 +1,10 @@
+class IconFontHelper {
+  static const String logo = 'a';
+  static const String mainLogo = 'b';
+  static const String meats = 'c';
+  static const String pastries = 'd';
+  static const String seeds = 'e';
+  static const String spices = 'f';
+  static const String vegs = 'g';
+  static const String fruits = 'h';
+}
